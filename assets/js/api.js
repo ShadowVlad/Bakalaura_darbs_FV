f837860478473157f3abd1b69486265eef124c5a
ShadowVlad <106116657+ShadowVlad@users.noreply.github.com>
2023-05-05 05:47:43 +0200

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -21,12 +21,12 @@ export const url = {
     return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
   },
   reverseGeo(lat, lon) {
-    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
   },
   /**
    * @param {string} query Meklē pieprasījumu piem: "Rīga".
    */
   geo(query) {
-    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
   }
 }
